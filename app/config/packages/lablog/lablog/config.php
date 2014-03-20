033aaeae157addf210caf4eaaa8f4debe84f1814
Joseph Ralph <joe@joescode.co.uk>
2014-03-20 23:38:11 +0000

    Initial Site Design Commit

---
+++ app/config/packages/lablog/lablog/config.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+    /**
+     * Register the prefix for the LaBlog system.
+     * Default: '/' (Eg. domain.com will go to your blog home.)
+     * Example: '/blog/' (Eg. domain.com/blog will go to your blog home.)
+     *
+     * Please remember to keep a slash at the end of the prefix!
+     */
+    'prefix' => '/',
+
+    /**
+     * Add any extra blog pages you wish to have here.
+     * Example: about, contact, terms_and_conditions
+     */
+    'pages' => array(
+        'plugins'
+    ),
+
+    /**
+     * Select a theme for the blog.
+     */
+    'theme' => 'lablog',
+
+);
